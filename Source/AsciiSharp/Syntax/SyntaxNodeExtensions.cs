5e4287fe4bd6d0ea6e9fd14f95db86c54ad5966f
aetos <aetos382@users.noreply.github.com>
2024-01-26 15:41:28 +0900

    wip

---
+++ Source/AsciiSharp/Syntax/SyntaxNodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AsciiSharp.Syntax;
+
+public static class SyntaxNodeExtensions
+{
+    public static T NormalizeWhitespace<T>(
+        this T node)
+        where T : SyntaxNode
+    {
+        ArgumentNullException.ThrowIfNull(node);
+
+        return node;
+    }
+}
