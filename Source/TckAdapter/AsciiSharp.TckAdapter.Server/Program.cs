84d68e0f3f49fb564e465dcc4422e95ec01db893
aetos <aetos382@users.noreply.github.com>
2024-02-02 18:24:40 +0900

    props

--- Source/TckAdapter/AsciiSharp.TckAdapter.Server/Program.cs
+++ Source/TckAdapter/AsciiSharp.TckAdapter.Server/Program.cs
@@ -1,3 +1,7 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,9 +21,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
