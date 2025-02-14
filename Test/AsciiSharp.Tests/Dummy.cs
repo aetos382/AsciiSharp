e666bc5dc245940bb9f9df6b0bb3c206a23e4ba5
aetos <aetos382@users.noreply.github.com>
2025-02-14 14:07:50 +0900

    add dummy test

---
+++ Test/AsciiSharp.Tests/Dummy.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsciiSharp.Tests;
+
+// テストケースが何かないとCIが失敗するのでダミーのテスト
+[TestClass]
+public sealed class Dummy
+{
+    [TestMethod]
+    public void X()
+    {
+    }
+}
