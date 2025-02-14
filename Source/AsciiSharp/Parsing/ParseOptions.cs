53779acec45456d889a571fbcb774b65939167a8
aetos <aetos382@users.noreply.github.com>
2025-02-14 14:18:16 +0900

    parser は preprocess しない

--- Source/AsciiSharp/Parsing/ParseOptions.cs
+++ Source/AsciiSharp/Parsing/ParseOptions.cs
@@ -2,18 +2,5 @@
 
 public class ParseOptions
 {
-    public ParseOptions()
-    {
-        this.FileResolver = new FileResolver(this);
-    }
-
-    public IFileResolver? FileResolver { get; set; }
-
-    public bool PreprocessDocument { get; set; }
-
-    public IDocumentPreprocessor[]? Preprocessors { get; set; }
-
-    public bool SkipTrivias { get; set; }
-
     public static readonly ParseOptions Default = new();
 }
