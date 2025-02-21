0921fd4f091eb24dcb16300c502c66985a22f905
aetos <aetos382@users.noreply.github.com>
2025-02-21 17:53:40 +0900

    add baseUri

--- Source/AsciiSharp/Parsing/ParseOptions.cs
+++ Source/AsciiSharp/Parsing/ParseOptions.cs
@@ -1,6 +1,10 @@
-﻿namespace AsciiSharp.Parsing;
+﻿using System;
+
+namespace AsciiSharp.Parsing;
 
 public class ParseOptions
 {
+    public Uri? BaseUri { get; set; }
+
     public static readonly ParseOptions Default = new();
 }
