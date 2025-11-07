65c8809cf80ab05652c961eb9e360220ed90169d
aetos <aetos382@users.noreply.github.com>
2025-11-08 00:26:55 +0900

    wip

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,4 @@
+uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
+
+git config --global --add safe.directory /workspaces/AsciiSharp/submodules/asciidoc-lang
+git config --global --add safe.directory /workspaces/AsciiSharp/submodules/asciidoc-tck
