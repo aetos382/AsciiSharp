f78898793cd09c6c1d11114b87852f349d437a99
aetos <aetos382@users.noreply.github.com>
2025-11-13 18:59:41 +0900

    remove uv

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,4 @@
-uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
+pip install --break-system-packages git+https://github.com/github/spec-kit.git
 
 git config --global --add safe.directory /workspaces/AsciiSharp/submodules/asciidoc-lang
 git config --global --add safe.directory /workspaces/AsciiSharp/submodules/asciidoc-tck
