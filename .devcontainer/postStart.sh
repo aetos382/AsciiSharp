6771e66a468732f3c1067e3e4ad56be5eb434757
aetos <aetos382@users.noreply.github.com>
2026-01-17 02:55:58 +0900

    restore

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -7,3 +7,5 @@ add_safe_directory '/workspaces/AsciiSharp/submodules/asciidoc-lang'
 add_safe_directory '/workspaces/AsciiSharp/submodules/asciidoc-tck'
 
 git submodule update --init
+
+dotnet restore
