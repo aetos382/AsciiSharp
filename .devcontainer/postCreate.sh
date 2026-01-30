3c45996283db6d1e19b732235afc0ef74f8e47bf
aetos <aetos382@users.noreply.github.com>
2026-01-31 01:47:24 +0900

    for gemini

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,8 @@
 mkdir -p /home/vscode/.local/bin
 mkdir -p /home/vscode/.dotnet/tools
 
+npm install -g @google/gemini-cli
+
 uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
 
 dotnet new install Reqnroll.Templates.DotNet
