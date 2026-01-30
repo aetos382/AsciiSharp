fe6fd2c1c0ce4e9fdeb2c1ae09e2031433b87fcd
aetos <aetos382@users.noreply.github.com>
2026-01-31 01:41:59 +0900

    remove claude

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,8 @@
 mkdir -p /home/vscode/.local/bin
 mkdir -p /home/vscode/.dotnet/tools
 
+npm install -g @openai/codex
+
 uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
 
 dotnet new install Reqnroll.Templates.DotNet
