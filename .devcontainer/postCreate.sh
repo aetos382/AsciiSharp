6319060407fea602e91ca343660035fd7aece036
aetos <aetos382@users.noreply.github.com>
2026-02-05 16:40:01 +0900

    install node and npm as user

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,3 +2,6 @@ uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
 
 dotnet new install Reqnroll.Templates.DotNet
 dotnet tool install -g csharp-ls
+
+npm install -g npm
+npm install -g @github/copilot @openai/codex @anthropic-ai/claude-code @google/gemini-cli
