283283552796b35900b2045be172bb545eb9d7e8
aetos <aetos382@users.noreply.github.com>
2026-02-19 01:50:56 +0900

    update

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,10 @@ dotnet tool install -g csharp-ls
 
 npm install -g npm
 npm install -g @anthropic-ai/claude-code
+
+# https://github.com/anthropics/claude-code/issues/19275
+cat .claude/settings.json |
+jq -r '.enabledPlugins | to_entries[] | select(.value == true) | .key' |
+while read -r plugin; do
+  claude plugin install "$plugin"
+done
